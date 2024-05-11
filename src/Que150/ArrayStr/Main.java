f4555e2c9be7e65bb34d8e75e196f43555e2de0b
cds007 <2205175277@qq.com>
2024-05-11 14:41:56 +0800

    arraystr new

---
+++ src/Que150/ArrayStr/Main.java
@@ -0,0 +1,18 @@
+package Que150.ArrayStr;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 4, 1, 5, 9};
+        String[] strArray = new String[]{"Z", "a", "D"};
+        Integer[] arrInteger = new Integer[arr.length];
+        Arrays.setAll(arrInteger,i -> arr[i]);
+        Arrays.sort(arrInteger,(a,b) -> b-a);
+        Arrays.setAll(arr,i->arrInteger[i]);
+        // 打印排序后的数组
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- src/huaweiExam/t1/Main.java
+++ src/huaweiExam/t1/Main.java
@@ -1,4 +1,4 @@
-package huaweiExam;
+package huaweiExam.t1;
 
 import java.util.HashMap;
 import java.util.Map;
